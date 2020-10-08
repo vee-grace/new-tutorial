60edc46edcdbdee812c6c6b76dc78b8942bbe278
vee-grace <72538983+vee-grace@users.noreply.github.com>
2020-10-08 13:45:42 +0800

    added a namn

--- hello.go
+++ hello.go
@@ -2,9 +2,13 @@ package main
 
 import "fmt"
 
+var (
+	myName string = "VERLIN"
+)
+
 func main() {
 	//make a program that lets the user input name
 
-	fmt.Print("Hello")
+	fmt.Printf("Hello %v", myName)
 
 }
